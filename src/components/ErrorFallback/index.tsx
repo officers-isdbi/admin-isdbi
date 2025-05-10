edc8590bd2878f25feabd21bda763a316115702c
Youcef Madadi <tahher@live.fr>
2025-05-10 22:18:29 +0100

    fixing size

--- src/components/ErrorFallback/index.tsx
+++ src/components/ErrorFallback/index.tsx
@@ -8,10 +8,12 @@ export default function ErrorFallback({
 }: FallbackProps) {
 	const navigate = useNavigate();
 	return (
-		<div className="flex h-full w-full items-center justify-center">
+		<div className="flex h-screen w-full items-center justify-center">
 			<div className="flex max-w-md flex-col items-center justify-center gap-10 py-4 text-center">
 				{/* <Logo className="max-h-48 w-[90vw] max-w-md" /> */}
-				<h1 className="text-8xl font-bold">Unexpected Error</h1>
+				<h1 className="text-8xl font-bold text-red-800 uppercase">
+					Unexpected Error
+				</h1>
 				<h2 className="text-3xl font-bold">something went wrong</h2>
 				<p>
 					{error && error instanceof Error
